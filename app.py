94eb0dd493d077d7949243b96cf270fc3a8b1362
Alexander Zakharchuk <mystang.pro@gmail.com>
2021-02-24 19:54:55 +0300

    Third task solution

--- app.py
+++ app.py
@@ -16,5 +16,12 @@ def training(prof):
         "training.html", title="Тренировка", prof=prof.lower())
 
 
+@app.route("/list_prof/<list>")
+def list_prof(list):
+    print(list)
+    return render_template(
+        "list_prof.html", title="Список профессий", list=list)
+
+
 if __name__ == '__main__':
     app.run(port=8080, debug=True)
